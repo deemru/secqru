9d88aef13239d4aa06784425d12ce3599842da34
Dmitrii Pichulin <deem@deem.ru>
2016-10-11 12:17:44 +0300

    - reworked routing to OSPF - reworked bridge filters - reworked keepalive

--- include/secqru_app_tiklan.php
+++ include/secqru_app_tiklan.php
@@ -46,10 +46,10 @@ class secqru_app_tiklan
         $g_psw_prefix = substr( sha1( $g_psw ), -8 ) . '_';
 
         // GLOBAL IP RANGE
-        $g_rng = $this->w->get_ip2long( 'g_rng:broadcast domain', ip2long( '192.168.160.0' ) );
+        $g_rng = $this->w->get_ip2long( 'g_rng:broadcast domain', ip2long( '172.17.0.0' ) );
 
         // GLOBAL IP CIDR (MASK)
-        $g_cidr = $this->w->get_int( 'g_cidr:broadcast cidr', 20, 16, 22 );
+        $g_cidr = $this->w->get_int( 'g_cidr:broadcast cidr', 20, 18, 22 );
 
         // SUBNET COUNTER INIT
         $temp = 0;
@@ -96,8 +96,7 @@ class secqru_app_tiklan
 
         // GLOBAL VPN
         $g_vpn = $this->w->get_dns( 'g_vpn:VPN protocol', 'L2TP' );
-        $vpn_protocols = array( 'L2TP' => 'l2tp', 'PPTP' => 'pptp',
-                                'SSTP' => 'sstp', 'OVPN' => 'ovpn' );
+        $vpn_protocols = array( 'L2TP' => 'l2tp', 'PPTP' => 'pptp', 'SSTP' => 'sstp' );
         if( !array_key_exists( $g_vpn, $vpn_protocols ) )
         {
             $g_vpn = 'L2TP';
@@ -130,8 +129,8 @@ class secqru_app_tiklan
                     break;
             }
 
-            // SUBNET DISTANCE (PRIORITY)
-            $subnets[$i]['dist'] = $this->w->get_int( "dist$i:router distance $i", 10, 1, 240 );
+            // SUBNET PATH COST
+            $subnets[$i]['cost'] = $this->w->get_int( "cost$i:router path cost $i", 10, 1, 999 );
 
             // SUBNET IP RANGE
             $subnets[$i]['subnet'] = $this->w->get_ip2long( "subnet$i", $g_rng + ( $i * 256 ) );
@@ -146,7 +145,7 @@ class secqru_app_tiklan
             // SUBNET PARAMETERS
             $subnets[$i]['addr_subnet'] = long2ip( $subnets[0]['subnet'] );
             $subnets[$i]['addr_gw'] = long2ip( $subnets[$i]['subnet'] + 1 );
-            $subnets[$i]['addr_id'] = ( $subnets[$i]['subnet'] >> 8 ) & 0xFF;
+            $subnets[$i]['addr_id'] = ( $subnets[$i]['subnet'] >> 8 ) - ( $subnets[0]['subnet'] >> 8 );
             $subnets[$i]['addr_vpn'] = long2ip( $subnets[0]['subnet'] + $subnets[$i]['addr_id'] );
             $subnets[$i]['addr_dhcp_first'] = long2ip( $subnets[$i]['subnet'] + 10 );
             $subnets[$i]['addr_dhcp_last'] = long2ip( $subnets[$i]['subnet_end'] );
@@ -159,7 +158,7 @@ class secqru_app_tiklan
         $temp = $g_eoip;
         for( $i = 1; $i <= $subnum; $i++ )
         {
-            $subnets[$i]['eoip_mark'] = $temp + $i;
+            $subnets[$i]['eoip_direct'] = $temp + $i;
             $temp += self::MAX_ROUTERS - $i - 2;
         }
 
@@ -225,7 +224,7 @@ class secqru_app_tiklan
         }
 
         // CHECK IP RANGE INTERSECTIONS
-        for( $i = 1; $i <= $subnum; $i++ )
+        for( $i = 0; $i <= $subnum; $i++ )
         {
             if( !isset( $subnets[$i]['subnet_ok'] ) )
                 $subnets[$i]['subnet_ok'] = true;
@@ -333,7 +332,7 @@ class secqru_app_tiklan
             $html->add( ' — address pool', 1 );
 
             $html->put_input_ro( self::FORMSIZE, long2ip( $subnets[0]['subnet'] + 1 ).' — '.long2ip( $subnets[0]['subnet_end'] ) );
-            $html->add( ' — VPN pool', 1 );
+            $html->add( ' — reserved pool', 1 );
 
             $html->put_input( 'g_psw', self::FORMSIZE, 50, $g_psw );
             $html->add( ' — password seed', 1 );
@@ -406,8 +405,8 @@ class secqru_app_tiklan
                 $html->put_submit( "sw_pub$i", $subnets[$i]['is_pub'] ? 'x' : 'v' );
                 $html->add( ' — public address', 1 );
 
-                $html->put_input( "dist$i", 3, 3, $subnets[$i]['dist'] );
-                $html->add( ' — router distance', 1 );
+                $html->put_input( "cost$i", 3, 3, $subnets[$i]['cost'] );
+                $html->add( ' — router path cost', 1 );
 
                 $html->input_full( 'text', "subnet$i", 15, 15, long2ip( $subnets[$i]['subnet'] ), $subnets[$i]['subnet_ok'] ? '' : 'e' );
                 $html->add( ' / ' );
@@ -441,14 +440,23 @@ class secqru_app_tiklan
     {
         $config = array();
 
+        $filter_list = $g_lan . '-Remotes';
+
         // GATEWAY OR NEWBIE
         if( !$g_sel || ( $g_sel && $g_sel == $i ) )
         {
-            // GW
-            $config[] = "# GW ({$subnets[$i]['name']})";
+            // Bridge
+            $config[] = "# Bridge ({$subnets[$i]['name']})";
             $config[] = "interface bridge add name=\"$g_lan\" comment=\"$g_lan\" protocol-mode=none mtu=1500";
             $config[] = "ip address add address={$subnets[$i]['addr_gw']}/$g_cidr interface=\"$g_lan\"";
-            $config[] = "ip route add dst-address={$subnets[0]['addr_subnet']}/24 type=unreachable distance=250";
+            $config[] = "ip route add dst-address={$subnets[0]['addr_subnet']}/24 type=unreachable";
+            $config[] = '';
+
+            // OSPF Routes
+            $config[] = "# OSPF Routes ({$subnets[$i]['name']})";
+            $config[] = "routing ospf instance add name=\"$g_lan\" router-id={$subnets[$i]['addr_vpn']}";
+            $config[] = "routing ospf area add name=\"$g_lan\" instance=\"$g_lan\" area-id={$subnets[0]['addr_subnet']}";
+            $config[] = "routing ospf network add network={$subnets[0]['addr_subnet']}/24 area=\"$g_lan\"";
             $config[] = '';
 
             // DHCP
@@ -458,17 +466,24 @@ class secqru_app_tiklan
             $config[] = "ip dhcp-server add name=\"{$subnets[$i]['dhcp_server_name']}\" interface=\"$g_lan\" address-pool=\"{$subnets[$i]['dhcp_pool_name']}\"";
             $config[] = "ip dhcp-server enable \"{$subnets[$i]['dhcp_server_name']}\"";
             $config[] = '';
+
+            // Bridge Filters
+            $config[] = "# Bridge Filters ({$subnets[$i]['name']})";
+            $config[] = "interface list add name=\"$filter_list\"";
+            $config[] = "interface bridge nat add chain=srcnat out-interface-list=\"$filter_list\" mac-protocol=ip dst-address=224.0.0.0/4 action=drop";
+            $config[] = "interface bridge nat add chain=srcnat out-interface-list=\"$filter_list\" mac-protocol=ipv6 action=drop";
+            $config[] = "interface bridge nat add chain=srcnat out-interface-list=\"$filter_list\" mac-protocol=ip ip-protocol=udp src-port=67-68 action=drop";
+            $config[] = '';
         }
 
         // PPP PROFILE ADD
-        $ppp_profile_name = 'default-encryption';
-        $ppp_timeout = '17';
+        $ppp_timeout = '7';
 
         // PPP SECRET ADD
         $ppp_users = array();
         $ppp_servers = array();
         $ppp_clients = array();
-        $ppp_routes = array();
+        $ospf_costs = array();
 
         for( $s = 1; $s <= $subnum; $s++ )
         {
@@ -485,7 +500,7 @@ class secqru_app_tiklan
                     }
                     else
                     {
-                        if( $subnets[$i]['dist'] > $subnets[$s]['dist'] || ( $subnets[$i]['dist'] == $subnets[$s]['dist'] && $i > $s ) )
+                        if( $subnets[$i]['cost'] > $subnets[$s]['cost'] || ( $subnets[$i]['cost'] == $subnets[$s]['cost'] && $i > $s ) )
                         {
                             $is_client = true;
                         }
@@ -503,14 +518,15 @@ class secqru_app_tiklan
                 if( $is_client )
                 {
                     $ppp_client_name = "$g_lan-$g_vpn-Client-{$subnets[$s]['name']}";
-                    $ppp_routes[] = "ip route add dst-address={$subnets[0]['addr_subnet']}/24 gateway=$ppp_client_name distance={$subnets[$s]['dist']}";
-                    $ppp_clients[] = "interface {$vpn_protocols[$g_vpn]}-client add connect-to=\"{$subnets[$s]['pub']}\" name=\"$ppp_client_name\" user=\"$g_lan-{$subnets[$i]['name']}\" password=\"{$subnets[$i]['psw']}\" profile=\"$ppp_profile_name\" keepalive-timeout=$ppp_timeout disabled=no";
+                    $ospf_costs[] = "routing ospf interface add interface=\"$ppp_client_name\" network-type=point-to-point cost={$subnets[$s]['cost']}";
+                    $ppp_clients[] = "interface {$vpn_protocols[$g_vpn]}-client add connect-to=\"{$subnets[$s]['pub']}\" name=\"$ppp_client_name\" user=\"$g_lan-{$subnets[$i]['name']}\" password=\"{$subnets[$i]['psw']}\" profile=default-encryption keepalive-timeout=$ppp_timeout disabled=no";
                     $ppp_clients[] = "ip neighbor discovery set \"$ppp_client_name\" discover=no";
                 }
                 else if( $is_server )
                 {
                     $ppp_server_name = "$g_lan-$g_vpn-Server-{$subnets[$s]['name']}";
-                    $ppp_users[] = "ppp secret add name=\"$g_lan-{$subnets[$s]['name']}\" password=\"{$subnets[$s]['psw']}\" profile=\"$ppp_profile_name\" local-address={$subnets[$i]['addr_vpn']} remote-address={$subnets[$s]['addr_vpn']} routes=\"{$subnets[0]['addr_subnet']}/24 {$subnets[$s]['addr_vpn']} {$subnets[$s]['dist']}\"";
+                    $ospf_costs[] = "routing ospf interface add interface=\"$ppp_server_name\" network-type=point-to-point cost={$subnets[$s]['cost']}";
+                    $ppp_users[] = "ppp secret add name=\"$g_lan-{$subnets[$s]['name']}\" password=\"{$subnets[$s]['psw']}\" profile=default-encryption local-address={$subnets[$i]['addr_vpn']} remote-address={$subnets[$s]['addr_vpn']}";
                     $ppp_servers[] = "interface {$vpn_protocols[$g_vpn]}-server add name=\"$ppp_server_name\" user=\"$g_lan-{$subnets[$s]['name']}\"";
                     $ppp_servers[] = "ip neighbor discovery set \"$ppp_server_name\" discover=no";
                 }
@@ -521,36 +537,45 @@ class secqru_app_tiklan
         if( sizeof( $ppp_users ) || sizeof( $ppp_servers ) )
         {
             $config[] = "# PPP Server ({$subnets[$i]['name']})";
-            $config[] = "interface {$vpn_protocols[$g_vpn]}-server server set enabled=yes";
+            $config[] = "interface {$vpn_protocols[$g_vpn]}-server server set enabled=yes keepalive-timeout=$ppp_timeout";
             $config = array_merge( $config, $ppp_users );
             $config =  array_merge( $config, $ppp_servers );
             $config[] = '';
         }
 
         // PPP Client
-        if( sizeof( $ppp_clients ) || sizeof( $ppp_routes ) )
+        if( sizeof( $ppp_clients ) )
         {
             $config[] = "# PPP Client ({$subnets[$i]['name']})";
             $config = array_merge( $config, $ppp_clients );
-            $config =  array_merge( $config, $ppp_routes );
+            $config[] = '';
+        }
+
+        // OSPF ROUTES WITH COSTS
+        if( sizeof( $ospf_costs ) )
+        {
+            $config[] = "# OSPF Costs ({$subnets[$i]['name']})";
+            $config = array_merge( $config, $ospf_costs );
             $config[] = '';
         }
 
         // EOIP ADD
         $eoip_interface = array();
-        $eoip_to_bridge = '';
-        $eoip_bridge_filter = '';
+        $eoip_to_bridge = array();
+        $eoip_bridge_filter = array();
+        $eoip_routes = array();
         for( $s = 1; $s <= $subnum; $s++ )
         {
             if( $i != $s && ( !$g_sel || ( $g_sel && $g_sel == $s ) || ( $g_sel && $g_sel == $i ) ) )
             {
-                $eoip_name = "$g_lan-EoIP-{$subnets[$s]['name']}";
-                $eoip_tunnel_id = $subnets[ min( $i, $s ) ]['eoip_mark'] + max( $i, $s ) - 2;
+                $eoip_name = "$g_lan--{$subnets[$s]['name']}";
+                $eoip_name_direct = "$g_lan-EoIP-{$subnets[$s]['name']}";
+                $eoip_id_direct = $subnets[ min( $i, $s ) ]['eoip_direct'] + max( $i, $s ) - 2;
 
-                $eoip_interface[] = "interface eoip add name=\"$eoip_name\" remote-address={$subnets[$s]['addr_vpn']} tunnel-id=$eoip_tunnel_id keepalive=7,3";
-                $eoip_interface[] = "ip neighbor discovery set \"$eoip_name\" discover=no";
-                $eoip_to_bridge[] = "interface bridge port add bridge=\"$g_lan\" interface=\"$eoip_name\" horizon=$g_horizon";
-                $eoip_bridge_filter[] = "interface bridge nat add chain=dstnat in-interface=\"$eoip_name\" mac-protocol=ip ip-protocol=udp src-port=67-68 action=drop";
+                $eoip_interface[] = "interface eoip add name=\"$eoip_name_direct\" !keepalive remote-address={$subnets[$s]['addr_vpn']} tunnel-id=$eoip_id_direct";
+                $eoip_interface[] = "ip neighbor discovery set \"$eoip_name_direct\" discover=no";
+                $eoip_to_bridge[] = "interface bridge port add bridge=\"$g_lan\" interface=\"$eoip_name_direct\" horizon=$g_horizon";
+                $eoip_bridge_filter[] = "interface list member add interface=\"$eoip_name_direct\" list=\"$filter_list\"";
             }
         }
 
@@ -562,7 +587,7 @@ class secqru_app_tiklan
         $config = array_merge( $config, $eoip_to_bridge );
         $config[] = '';
 
-        $config[] = "# EoIP filter DHCP ({$subnets[$i]['name']})";
+        $config[] = "# EoIP Filters ({$subnets[$i]['name']})";
         $config = array_merge( $config, $eoip_bridge_filter );
 
         return $config;
